d7f07a479033da7286539ee5c357b6b306dc025e
harish-r <harish23r@gmail.com>
2018-11-25 18:59:58 -0700

    added near completed game

--- infinity-roll-a-ball/Assets/Scripts/BridgeManager.cs
+++ infinity-roll-a-ball/Assets/Scripts/BridgeManager.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BridgeManager : MonoBehaviour {
+
+    //const int ANSWERBOARD = 2;
+    //const int OPERATOR = 1;
 
     // Use this for initialization
     public GameObject[] bridgePreFabs;
@@ -12,8 +17,38 @@ public class BridgeManager : MonoBehaviour {
     private float bridgeLength = 10.0f;
     private int bridgeAmount = 2;
 
-	void Start () {
+    string[] operators = new string[] {"+", "-", "x", "/"};
+    public int bridgeCount;
+    public static List<int> operandList1;
+    public static List<int> operandList2;
+    public static List<int> allOperands;
 
+    System.Random random = new System.Random();
+    int numDigits = 1;
+    int finalAnswer = 0;
+
+    List<int> generateNumbers() {
+        Debug.Log("GeneratingNumbers");
+
+        List<int> numberList = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            int num = 0;
+            while (num == 0)
+                num = random.Next(10 * (numDigits - 1), (10 * numDigits) - 1);
+            allOperands.Add(num);
+            numberList.Add(num);
+            //Debug.Log(numberList[i]);
+        }
+        return numberList;
+    }
+
+
+    void Start () {
+        bridgeCount = 1;
+        allOperands = new List<int>();
+        operandList1 = new List<int>();
+        operandList2 = new List<int>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         for (int i = 0; i < bridgeAmount; i++){
             SpawnBridge();
@@ -21,7 +56,21 @@ public class BridgeManager : MonoBehaviour {
        
 
     }
-	
+
+    public List<GameObject> FindRollPlanes(Transform parent, string tag) {
+        List<GameObject> rollPlanes = new List<GameObject>();
+        for (int i = 0; i < parent.childCount; i++) {
+            Transform child = parent.GetChild(i);
+            if (child.tag == tag) {
+                rollPlanes.Add(child.gameObject);
+            }
+            if (child.childCount > 0) {
+                rollPlanes.AddRange(FindRollPlanes(child, tag));
+            }
+        }
+        return rollPlanes;
+    }
+
 	// Update is called once per frame
 	void Update () {
         if(playerTransform.position.z > (spawnZ - bridgeAmount * bridgeLength)){
@@ -33,10 +82,58 @@ public class BridgeManager : MonoBehaviour {
 
         GameObject go;
         go = Instantiate(bridgePreFabs[0]) as GameObject;
+        // Get all roll planes in the game object
+        List<GameObject> rollplanes = FindRollPlanes(go.transform, "rollplane");
+        Debug.Log("Rollplanes count: " + rollplanes.Count);
+
+        if (bridgeCount == 1 || bridgeCount == 3)
+        {
+            List<int> numbers = generateNumbers();
+            if (bridgeCount == 1) {
+                operandList1 = new List<int>(numbers);
+            }
+            else if (bridgeCount == 3) {
+                operandList2 = new List<int>(numbers);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Debug.Log(rollplanes[i]);
+                Debug.Log(rollplanes[i].transform.childCount);
+                GameObject childObject = rollplanes[i].transform.GetChild(0).gameObject;
+                TextMeshPro textMeshPro = (TextMeshPro)childObject.GetComponent("TextMeshPro");
+                textMeshPro.SetText(numbers[i].ToString());
+            }
+        }
+        else if (bridgeCount == 2) {
+            for (int i = 0; i < 4; i++)
+            {
+                Debug.Log(rollplanes[i]);
+                Debug.Log(rollplanes[i].transform.childCount);
+                GameObject childObject = rollplanes[i].transform.GetChild(0).gameObject;
+                TextMeshPro textMeshPro = (TextMeshPro)childObject.GetComponent("TextMeshPro");
+                textMeshPro.SetText(operators[i]);
+            }
+        }
+        else if (bridgeCount == 4) {
+            for (int i = 0; i < 4; i++) {
+                rollplanes[i].tag = "answerboard";
+            }
+        }
+
+        if (bridgeCount >= 4)
+        {
+            bridgeCount = 1;
+            //BridgeManager.allOperands.Clear();
+        }
+        else
+            bridgeCount++;
+        Debug.Log("Bridge count: " + bridgeCount);
+        
+
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += bridgeLength;
-
          
     }
 }
--- infinity-roll-a-ball/Assets/Scripts/PlayerController.cs
+++ infinity-roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,195 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+//using static BridgeManager;
 
 public class PlayerController : MonoBehaviour {
 
     // Use this for initialization
     public float speed;
 
+    private Stack operands;
+
+    private int expectedResultIndex;
+    private int expectedResult;
+
+    private string[] operatorList = new string[4] { "+", "-", "x", "/", };
+
+    private int[] answerOptions = new int[4];
+
     private Rigidbody rb;
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
-	
+        operands = new Stack();	
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public int ComputeResult(int operand1, int operand2, string operation)
+    {
+
+        int result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                result = operand1 + operand2;
+                break;
+
+            case "-":
+                result = operand1 - operand2;
+                break;
+
+            case "x":
+                result = operand1 * operand2;
+                break;
+
+            case "/":
+                result = operand1 / operand2;
+                break;
+
+            default:
+                Debug.Log("Invalid operator");
+                break;
+        }
+        return result;
+    }
+
+    public int GetExpectedResult()
+    {
+        int operand2 = Convert.ToInt32(operands.Pop());
+        string operation = (string)operands.Pop();
+        int operand1 = Convert.ToInt32(operands.Pop());
+
+        
+        return ComputeResult(operand1, operand2, operation);
+    }
+
+    public void GenerateAnswerOptions()
+    {
+        
+        Debug.Log("Generatint Options");
+        System.Random random = new System.Random();
+        expectedResultIndex = random.Next(0, 4);
+        answerOptions[expectedResultIndex] = GetExpectedResult();
+        expectedResult = answerOptions[expectedResultIndex];
+
+        Debug.Log(BridgeManager.allOperands);
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == expectedResultIndex)
+                continue;
+            int randomOperandIndex1 = random.Next(8);
+            int randomOperandIndex2 = random.Next(8);
+            int randomOperatorIndex = random.Next(4);
+
+            Debug.Log(randomOperandIndex1);
+            Debug.Log(randomOperandIndex2);
+            Debug.Log(randomOperatorIndex);
+
+            answerOptions[i] = ComputeResult(BridgeManager.allOperands[randomOperandIndex1], BridgeManager.allOperands[randomOperandIndex2], operatorList[randomOperatorIndex]);
+            Debug.Log("Generated Option " + answerOptions[i]);
+            i++;
+        }
+
+    }
+
+    public void setAnswerOptionsintoPlane()
+    {
+        Debug.Log("Setting Options");
+        GameObject[] answerBoards = GameObject.FindGameObjectsWithTag("answerboard");
+        Debug.Log("Answer baord lenfht:" + answerBoards.Length);
+        for (int i = 0; i < answerBoards.Length; i++)
+        {
+            TextMeshPro textMeshPro = GetTextMeshReference(answerBoards[i]);
+            textMeshPro.SetText(answerOptions[i].ToString());
+        }
+    }
+
+    public TextMeshPro GetTextMeshReference(GameObject gameObject)
+    {
+        GameObject childObject = gameObject.transform.GetChild(0).gameObject;
+        TextMeshPro textMeshPro = (TextMeshPro)childObject.GetComponent("TextMeshPro");
+        return textMeshPro;
+    }
+
+    public string GetRollPlaneValue(GameObject gameObject)
+    {
+        TextMeshPro textMeshPro = GetTextMeshReference(gameObject);
+        string value = (string)textMeshPro.text;
+        return value;
+    }
+
+    public string CollectRollOverAndDestroy(Collision collision)
+    {
+        string value;
+        value = GetRollPlaneValue(collision.gameObject);
+        Destroy(collision.gameObject);
+        return value;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        string value;
+
+        if (collision.gameObject.CompareTag("rollplane"))
+        {
+            //UpdateTime();
+
+            value = CollectRollOverAndDestroy(collision);
+
+            Debug.Log("Picked the value " + value);
+
+            operands.Push(value);
+
+            Debug.Log("Operand count: " + operands.Count);
+
+            if (operands.Count == 3)
+            {
+                GenerateAnswerOptions();
+                setAnswerOptionsintoPlane();
+            }
+        }
+
+        if (collision.gameObject.CompareTag("answerboard"))
+        {
+            //UpdateTime();
+
+            Debug.Log("Collided with answer board");
+
+            value = CollectRollOverAndDestroy(collision);
+
+            int chosenResult = Convert.ToInt32(value);
+            Debug.Log("Chosen Result " + chosenResult);
+            Debug.Log("Correct Result " + answerOptions[expectedResultIndex]);
+
+            if (chosenResult == expectedResult)
+            {
+                Debug.Log("Win");
+            }
+            else
+            {
+                Debug.Log("Lose");
+            }
+
+            GameObject[] answerboards = GameObject.FindGameObjectsWithTag("answerboard");
+            for (int i = 0; i < answerboards.Length; i++) {
+                Destroy(answerboards[i]);
+            }
+
+
+
+        }
+        
+    }
+
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
